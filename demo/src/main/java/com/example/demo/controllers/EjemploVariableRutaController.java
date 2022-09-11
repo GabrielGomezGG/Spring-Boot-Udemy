84201705a6682ba404b1ded056006fd00421406e
GabrielGomezGG <gabrielgomezgg1997@gmail.com>
2022-09-11 18:30:36 -0300

    21 @PathVariable multiple variables + th:src

--- demo/src/main/java/com/example/demo/controllers/EjemploVariableRutaController.java
+++ demo/src/main/java/com/example/demo/controllers/EjemploVariableRutaController.java
@@ -10,9 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/variables")
 public class EjemploVariableRutaController {
 
+    @GetMapping("/")
+    public String index(Model model){
+        return "variables/index";
+    }
+
     @GetMapping("/string/{texto}")
     public String variables(@PathVariable String texto, Model model){
         model.addAttribute("resultado", "El resultado es: "+texto);
+        return "variables/ver";
+    }
+
+    @GetMapping("/string/{texto}/{numero}")
+    public String variables(@PathVariable String texto, @PathVariable Integer numero, Model model){
+        model.addAttribute("resultado", "El resultado es: "+texto + " y el numero es: " + numero);
         return "variables/ver";
     }
 }
--- demo/src/main/java/com/example/demo/controllers/IndexController.java
+++ demo/src/main/java/com/example/demo/controllers/IndexController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 

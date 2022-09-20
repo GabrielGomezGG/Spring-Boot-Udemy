48ef5ce6c30697274a4d276a303cb8a96e4bf369
GabrielGG <gabrielgomezgg1997@gmail.com>
2022-09-20 14:32:56 -0300

    58 @Email @Size

--- demoForm/src/main/java/com/example/demoform/models/entity/Usuario.java
+++ demoForm/src/main/java/com/example/demoform/models/entity/Usuario.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -22,9 +24,11 @@ public class Usuario {
     private String apellido;
 
     @NotEmpty
+    @Size(min = 3, max = 10)
     private String username;
     @NotEmpty
     private String password;
     @NotEmpty
+    @Email
     private String email;
 }

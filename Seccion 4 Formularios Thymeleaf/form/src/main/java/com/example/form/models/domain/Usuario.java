e47afc1692babeb9b3acede4c1aabd5e93cb4a57
GabrielGG <gabrielgomezgg1997@gmail.com>
2023-01-12 18:38:48 -0300

    61. Validación personalizada usando anotación @Pattern para expresiones regulares

--- Seccion 4 Formularios Thymeleaf/form/src/main/java/com/example/form/models/domain/Usuario.java
+++ Seccion 4 Formularios Thymeleaf/form/src/main/java/com/example/form/models/domain/Usuario.java
@@ -2,6 +2,7 @@ package com.example.form.models.domain;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import lombok.Setter;
 @Setter
 public class Usuario {
 
+    @Pattern(regexp = "[0-9]{2}[.][0-9]{3}[.][0-9]{3}[-][A-Z]")
     private String id;
 
     @NotEmpty(message = "nombre incorrecto")

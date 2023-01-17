f66a63862045233a577c30d31f80aa8ae4bf258e
GabrielGG <gabrielgomezgg1997@gmail.com>
2023-01-17 10:24:07 -0300

    91. Introduccón a los interceptores Http

---
+++ Seccion 4 Formularios Thymeleaf/form/src/main/java/com/example/form/interceptors/TiempoTrascurridoInterceptor.java
@@ -0,0 +1,21 @@
+package com.example.form.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class TiempoTrascurridoInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+}

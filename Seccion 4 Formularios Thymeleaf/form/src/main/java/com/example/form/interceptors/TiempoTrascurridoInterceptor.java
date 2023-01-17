480b00f8dfee79b8f2f08377b1c55bd2c711198c
GabrielGG <gabrielgomezgg1997@gmail.com>
2023-01-17 11:15:33 -0300

    94. Implementando fragment interceptor para reutilizar código en thymeleaf

--- Seccion 4 Formularios Thymeleaf/form/src/main/java/com/example/form/interceptors/TiempoTrascurridoInterceptor.java
+++ Seccion 4 Formularios Thymeleaf/form/src/main/java/com/example/form/interceptors/TiempoTrascurridoInterceptor.java
@@ -20,6 +20,12 @@ public class TiempoTrascurridoInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        /*
+        if(request.getMethod().equalsIgnoreCase("post")){
+            return true;
+        }
+         */
+
         if(handler instanceof  HandlerMethod){
             HandlerMethod metodo = (HandlerMethod) handler;
             logger.info("Es un metodo del controlador: ".concat(metodo.getMethod().getName()));
@@ -32,7 +38,7 @@ public class TiempoTrascurridoInterceptor implements HandlerInterceptor {
         request.setAttribute("tiempoInicio", tiempoInicio);
 
         Random random = new Random();
-        Integer demora = random.nextInt(500);
+        Integer demora = random.nextInt(100);
         Thread.sleep(demora);
 
         return true;

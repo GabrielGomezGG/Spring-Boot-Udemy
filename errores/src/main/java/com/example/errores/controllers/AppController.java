1df31911788cc144838f969fceacf634788e36a6
GabrielGG <gabrielgomezgg1997@gmail.com>
2023-01-17 15:36:09 -0300

    109. Añadiendo método handler ver

--- errores/src/main/java/com/example/errores/controllers/AppController.java
+++ errores/src/main/java/com/example/errores/controllers/AppController.java
@@ -1,16 +1,32 @@
 package com.example.errores.controllers;
 
+import com.example.errores.models.domain.Usuario;
+import com.example.errores.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AppController {
+
+    @Autowired
+    private UsuarioService usuarioService;
 
     @GetMapping("/index")
     public String index(Model model){
         //Integer algo = 100/0;
         Integer algo = Integer.parseInt("10xc");
         return "index";
+    }
+
+    @GetMapping("/ver/{id}")
+    public String ver(@PathVariable Integer id, Model model){
+
+        Usuario usuario = usuarioService.getUsuarioForId(id);
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Detalle del usuario: ".concat(usuario.getNombre()));
+        return "ver";
     }
 }

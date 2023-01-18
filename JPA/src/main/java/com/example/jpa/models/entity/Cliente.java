1290afb972ef0069adae0f2bc94e7a0fbb9b39f8
GabrielGG <gabrielgomezgg1997@gmail.com>
2023-01-18 11:46:43 -0300

    114. Creando la clase Entidad JPA anotada con @Entity

---
+++ JPA/src/main/java/com/example/jpa/models/entity/Cliente.java
@@ -0,0 +1,28 @@
+package com.example.jpa.models.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    @Column(name = "creado_en")
+    @Temporal(TemporalType.DATE)
+    private Date creadoEn;
+
+}

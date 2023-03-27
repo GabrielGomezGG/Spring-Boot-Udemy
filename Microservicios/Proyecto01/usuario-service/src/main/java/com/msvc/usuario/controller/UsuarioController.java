2d6e2c1e9f7363217ef3b64941f15754032eaa28
GabrielGomezGG <43209264+GabrielGomezGG@users.noreply.github.com>
2023-03-27 14:54:59 -0300

    31. Implementando Circuit Breaker con Resilience4j - Parte 3

--- Microservicios/Proyecto01/usuario-service/src/main/java/com/msvc/usuario/controller/UsuarioController.java
+++ Microservicios/Proyecto01/usuario-service/src/main/java/com/msvc/usuario/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.msvc.usuario.controller;
 
 import com.msvc.usuario.entity.Usuario;
 import com.msvc.usuario.service.UsuarioService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,16 +23,29 @@ public class UsuarioController {
         return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getUsuarioPorId(@PathVariable String id){
-        var usuario = usuarioService.getUsuario(id);
-        return ResponseEntity.ok(usuario);
-    }
+
     
     @GetMapping()
     public ResponseEntity<List<Usuario>> getAllUsuarios (){
         var usuarios = usuarioService.getAllUsuarios();
         return ResponseEntity.ok(usuarios);
+    }
+
+    @GetMapping("/{id}")
+    @CircuitBreaker(name="ratingHotelFallback", fallbackMethod = "ratingHotelFallback")
+    public ResponseEntity<Usuario> getUsuarioPorId(@PathVariable String id){
+        var usuario = usuarioService.getUsuario(id);
+        return ResponseEntity.ok(usuario);
+    }
+
+    public ResponseEntity<Usuario> ratingHotelFallback(String usuarioId, Exception exception){
+        var usuario = Usuario.builder()
+                .nombre("root")
+                .email("asasd@gmail.com")
+                .informacion("Se cayo el sirvicio")
+                .id("1234")
+                .build();
+        return ResponseEntity.ok(usuario);
     }
 
 }
--- Microservicios/Proyecto01/usuario-service/src/main/java/com/msvc/usuario/entity/Usuario.java
+++ Microservicios/Proyecto01/usuario-service/src/main/java/com/msvc/usuario/entity/Usuario.java
@@ -12,6 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 @Table(name = "usuarios")
 public class Usuario {
 
